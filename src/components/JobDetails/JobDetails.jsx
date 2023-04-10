0c85387afe3aadf6be00a6f84d20bc7382ba89c2
Yeasir Arafat <ysarafat.bd@gmail.com>
2023-04-10 07:15:07 +0600

    jobs details json data create and check

--- src/components/JobDetails/JobDetails.jsx
+++ src/components/JobDetails/JobDetails.jsx
@@ -1,8 +1,14 @@
 import React from "react";
 const JobDetails = ({ job }) => {
+  const { logo, title, company, type, hours, location, salary } = job;
   return (
     <div>
-      <img src={job.logo} alt="" />
+      <img className="w-[300px] h-14 object-fit" src={logo} alt="" />
+      <h2>{title}</h2>
+      <p>{company}</p>
+      <div>
+        <button></button>
+      </div>
     </div>
   );
 };
