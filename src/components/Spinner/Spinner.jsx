3458ba2b561ae4a4473ad8004d7e12056c244fb2
Yeasir Arafat <ysarafat.bd@gmail.com>
2023-04-28 18:28:31 +0600

    Spinner issue fixed

--- src/components/Spinner/Spinner.jsx
+++ src/components/Spinner/Spinner.jsx
@@ -2,9 +2,9 @@ import React from "react";
 
 const Spinner = () => {
   return (
-    <div className=" flex justify-center items-center h-[calc(100vh-542px)]">
+    <div className=" flex justify-center items-center min-h-screen">
       <div className="border-8 border-dashed animate-spin  border-primary h-24 w-24 rounded-full"></div>
-
+     
       {/* <p className="text-7xl text-primary ">Lodding<span className="animate-ping">...</span></p> */}
     </div>
   );
